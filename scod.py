06a4212ea373535afe74614f301f42683fa53a29
hajar1380 <hajar.na3eri@gmail.com>
2025-04-29 23:14:51 +0330

    add show-menue def to scod.py

--- scod.py
+++ scod.py
@@ -2,6 +2,9 @@
 products=[{"id": 1, "name":"roz Abi","price":170,"stock":300 ,"takhfif":},
 {"id": 2, "name":"roz Sefid","price":100,"stock":100},"takhfif":10%]
 
+def shoe_menue():
+    print("show all products ")
+    
 def shoe_products():
     print("/n products list")
     for product in products:
